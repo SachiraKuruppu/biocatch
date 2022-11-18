f84f7269796f693cd6a63d7bc1b4ff7296310949
rsachira-abi <rsachira@live.com>
2022-11-19 09:02:51 +1300

    Logout functionality added

---
+++ pages/api/logout.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { serialize } from 'v8';
+import { getSerializedCookie } from '../../lib/auth';
+
+export default function Logout(req: NextApiRequest, res: NextApiResponse) {
+  const { cookies } = req;
+  const jwt = cookies.session;
+
+  if (jwt) {
+    const serializedCookie = getSerializedCookie('');
+    res.setHeader('Set-Cookie', serializedCookie);
+  }
+
+  return res.status(200).json({ message: 'Success' });
+}
