b6b24c08f501f8bb5a76c8dde42137637029def1
rsachira-abi <rsachira@live.com>
2022-11-20 01:12:02 +1300

    Dashboard with user email created

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { getUserFromToken } from '../lib/auth';
 
-export default function Dashboard() {
+export async function getServerSideProps(context: any) {
+  const { cookies } = context.req;
+  const jwt = cookies['session'];
+
+  const user = await getUserFromToken(jwt);
+  console.log(user);
+
+  return { props: user };
+}
+
+export default function Dashboard({ email }: { email: string }) {
   return (
     <main>
+      <p>Hi {email},</p>
       <p>This is a mock dashboard with user account info</p>
       <button>Make Payment</button>
     </main>
