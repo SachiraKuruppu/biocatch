393caf99333fa61a79478ccf5a7214d49c5d8a5a
rsachira-abi <rsachira@live.com>
2022-11-20 21:51:36 +1300

    Navigation bar changed according to the login status

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,28 +1,18 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
+import { LoginContext } from '../../client-lib/login-context';
 import styles from '../../styles/Navbar.module.css';
 
 export default function Navbar() {
-  const router = useRouter();
+  const { isLoggedIn } = useContext(LoginContext);
 
   return (
     <div className={styles.container}>
       <Link href="/">Home</Link>
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/login">Login</Link>
-      <Link
-        href=""
-        onClick={async () => {
-          const response = await fetch('/api/logout', {
-            method: 'POST'
-          });
-
-          router.push('/');
-        }}
-      >
-        Logout
-      </Link>
+      {isLoggedIn && <Link href="/dashboard">Dashboard</Link>}
+      {!isLoggedIn && <Link href="/login">Login</Link>}
+      {isLoggedIn && <Link href="/logout">Logout</Link>}
     </div>
   );
 }
